9a2f9f91fa84ff0dcbd99ff0a7cf193665d2baee
Quinn Peterson <qap2001@gmail.com>
2022-10-17 15:35:09 -0700

    still init setup

---
+++ src/MyApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyApp() {
+    return (
+        <div className="container">
+            <>Hello WORLD</>
+        </div>
+    );
+}
+
+export default MyApp;
