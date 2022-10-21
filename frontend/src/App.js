ae5cecc2f85f0fb85e62a5c6130222113c6b789e
DJump13 <djump2019@gmail.com>
2022-10-21 15:57:20 -0700

    installed bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return ( 
       <div> 
-         <h1>Hello, React!</h1> 
+         <h1>Hello, React!</h1>
+         <Button>Test Button</Button> 
       </div> 
     ); 
 }
