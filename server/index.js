3e92748ac8d12fb7d0aa781d68c27f6bf770b4a6
Dominic Davidson <domdavidson251@gmail.com>
2022-10-21 14:42:41 -0700

    test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 app.use(express.json());
+//this is weird
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
