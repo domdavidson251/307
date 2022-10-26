ea126c876ed19a7cae8ec32bcc1fb0d52e115b52
DJump13 <djump2019@gmail.com>
2022-10-26 15:48:20 -0700

    basic form on submit review page, not working properly

--- frontend/src/submit-review.js
+++ frontend/src/submit-review.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import HeaderComp from "./header";
+import Form from './Form';
+import "./assets/css/style.css"
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const SubmitReview = () => {
     return (
@@ -8,8 +13,29 @@ const SubmitReview = () => {
         <div>
           <p>submit review page!</p>
         </div>
-      </div>
-    );
+        <Navbar className="color-nav" variant="dark">
+          <Container>
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link href="#home">Home</Nav.Link>
+              </LinkContainer>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <LinkContainer to="/submit-review">
+                <Nav.Link>Submit Review</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Container>
+        </Navbar>
+        
+        <div className="container">
+          {/*<Table characterData={characters} removeCharacter={removeOneCharacter} />*/}
+          <Form />
+        </div>
+        <div><p>will prob make a header component and put at top of all pages
+            instead of having copied html in every component</p>SUBMIT REVIEW PAGE </div>
+        </div>
+    )
 }
  
 export default SubmitReview;
