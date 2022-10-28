0ae25963b26569ce80a92f5940fac721cf7da101
Dominic Davidson <domdavidson251@gmail.com>
2022-10-28 15:31:13 -0700

    database schemas established

--- backend/backend.js
+++ backend/backend.js
@@ -9,6 +9,18 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+//get restaurants
+// get restaurant by id
+// get restaurant by name
+// get menu items
+// get menu item by id
+//get ratings
+//post(add) rating
+//post(add) menu item
+//post(add) restaurant
+//delete rating
+//delete restaurant
+//delete menu item
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
---
+++ backend/models/schemas.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const RatingSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    stars: Number,
+    review: String,
+    date: String,
+    author: String,
+    upvotes: Number,
+    downvotes: Number,
+    menuitem: {type: mongoose.Types.ObjectId, ref: 'MenuItem'},
+    restaurant: {type: mongoose.Types.ObjectId, ref: 'Restaurant'}
+}, {collection : 'ratings_list'});
+
+const MenuItemSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: String,
+    price: Number,
+    description: String,
+    restaurant: {type: mongoose.Types.ObjectId, ref: 'Restaurant'},
+    ratings: [{type: mongoose.Types.ObjectId, ref: 'Rating'}]
+}, {colection : 'menuitems_list'});
+
+const RestaurantSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: String,
+    location: String,
+    category: String,
+    image: String,
+    ratings: [{type: mongoose.Types.ObjectId, ref: 'Rating'}],
+    menuitems: [{type: mongoose.Types.ObjectId, ref: 'MenuItem'}]
+}, {collection : 'restaurants_list'});
+
+const rating = mongoose.model('Rating', RatingSchema);
+const menuitem = mongoose.model('Menu Item', MenuItemSchema);
+const restaurant = mongoose.model('Restaurant', RestaurantSchema)
+
+
+module.exports = RestaurantSchema;
+module.exports = MenuItemSchema;
+module.exports = RatingSchema;
