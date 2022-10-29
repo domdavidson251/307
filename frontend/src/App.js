82c8de32121444de0dece8d21925d8cffed96503
Quinn Peterson <qap2001@gmail.com>
2022-10-29 13:33:55 -0700

    connect frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeaderComp from './header';
+import axios from 'axios';
 
 function App() {
-    return (
+  const [testStateVar, setTestStateVar] = useState();
+
+  useEffect(() => {
+    fetchAll().then(result => {
+      if (result) { 
+        setTestStateVar(result);
+      }
+    });
+  }, [testStateVar]);
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:4000/');
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error); 
+      return false;         
+    }
+  }
+  
+  return (
+    <div>
+      <HeaderComp></HeaderComp>
       <div>
-        <HeaderComp></HeaderComp>
-        <div>
-          <p>HOME PAGEEEE</p>
-        </div>
+        <p>HOME PAGEEEE</p>
       </div>
-    ); 
+    </div>
+  ); 
 }
 
 export default App;
