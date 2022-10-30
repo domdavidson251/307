c40cd3c4fd6c006860a49766fed1b9304c6c5549
Quinn Peterson <qap2001@gmail.com>
2022-10-30 15:44:45 -0700

    cleanup App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeaderComp from './header';
-import axios from 'axios';
 
 function App() {
-  const [testStateVar, setTestStateVar] = useState();
   const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
-
     // temporary fake data for restaurants 
     setRestaurants(
       [
@@ -30,13 +27,7 @@ function App() {
         },
       ]
     )
-
-    fetchAll().then(result => {
-      if (result) { 
-        setTestStateVar(result);
-      }
-    });
-  }, [testStateVar]);
+  }, []);
 
   function makeTableBody() {
     const eles = restaurants.map((restaurant, index) => {
@@ -53,17 +44,6 @@ function App() {
       );
     });
     return eles;
-  }
-
-  async function fetchAll(){
-    try {
-      const response = await axios.get('http://localhost:4000/');
-      console.log(response);
-      return response;
-    } catch (error) {
-      console.log(error); 
-      return false;         
-    }
   }
   
   return (
