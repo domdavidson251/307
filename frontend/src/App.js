ac4a4345a5283766e086eb4cd98e70c601122313
DJump13 <djump2019@gmail.com>
2022-10-30 14:17:43 -0700

    better cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,27 +27,94 @@ function App() {
   
   return (
     <div>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      
       <HeaderComp></HeaderComp>
-      <div class="row">
-        <div class="col-sm-6">
-        <div class="card">
-          {//<img class="card-img-top" src="..." alt="Card image cap">
-}
-          <div class="card-body">
-            <p class="card-text">Restaurant1</p>
-          </div>
+      <div class="container mt-4">
+        <div class="row">
+            <div class="col-auto mb-3">
+                <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+              <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+              <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+                <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+                <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+                <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto mb-3">
+                <div class="card" style={{width: '18rem'}}>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {/*<a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>*/}
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-        <div class="col-sm-6">
-        <div class="card">
-          {//<img class="card-img-top" src="..." alt="Card image cap">
-}
-          <div class="card-body">
-            <p class="card-text">Restaurant2</p>
-          </div>
-        </div>
-        </div>
-      </div>
+    </div>
     </div>
   ); 
 }
