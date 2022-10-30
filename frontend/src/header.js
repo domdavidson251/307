3e1f55e6a85068f1760483eeb317ade2f7a66502
Quinn Peterson <qap2001@gmail.com>
2022-10-30 14:22:14 -0700

    restyle all pages by restyling header

--- frontend/src/header.js
+++ frontend/src/header.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const HeaderComp = () => {
     return (
         <div>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
             <div className="header">
                 <h1>Learn By Dining</h1>
                 <p>Find the Best Food on Campus</p>
