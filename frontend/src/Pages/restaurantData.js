aeca1becceb842bb602da229aa4673a41ef3e1ad
Aria-ista-0819 <aalva129@calpoly.edu>
2022-11-01 23:06:19 -0700

    Adjusted src layout and worked on home page

---
+++ frontend/src/Pages/restaurantData.js
@@ -0,0 +1,21 @@
+const restaurantData = [
+    {
+        img: '/images/brunch.jpg',
+        name: 'Brunch',
+    },
+    {
+        img: '/images/subway.jpg',
+        name: 'Subway',
+    },
+    {
+        img: '/images/chickFillA.jpg',
+        name: 'Chick-Fil-A',
+    },
+    {
+        img: '/images/streats.jpg',
+        name: 'Streats',
+    }
+
+];
+
+export default restaurantData;
