92a77d6f38d1afe210088b0d1e275969924ff112
Aria-ista-0819 <aalva129@calpoly.edu>
2022-11-09 15:13:13 -0800

    added more images

--- frontend/src/Pages/restaurantData.js
+++ frontend/src/Pages/restaurantData.js
@@ -1,19 +1,43 @@
 const restaurantData = [
   {
-    img: "/images/brunch.jpg",
+    img: "/images/balance-cafe-logo.jpg",
+    name: "Balance Cafe",
+  },
+  {
+    img: "/images/brunch-logo.jpg",
     name: "Brunch",
   },
   {
-    img: "/images/subway.jpg",
-    name: "Subway",
+    img: "/images/hearth-logo.jpg",
+    name: "Hearth",
   },
   {
-    img: "/images/chickFillA.jpg",
-    name: "Chick-Fil-A",
+    img: "/images/jamba-logo.jpg",
+    name: "Jamba Juice",
   },
   {
-    img: "/images/streats.jpg",
+    img: "/images/market-grand-ave-logo.jpg",
+    name: "Market Grand Ave",
+  },
+  {
+    img: "/images/Mingle-Nosh-Circle.jpg",
+    name: "M N Circle",
+  },
+  {
+    img: "/images/noodles-logo.jpg",
+    name: "Noodles",
+  },
+  {
+    img: "/images/streats-logo.jpg",
     name: "Streats",
+  },
+  {
+    img: "/images/sweet-bar-logo.jpg",
+    name: "Sweet Bar",
+  },
+  {
+    img: "/images/vg-logo.jpg",
+    name: "Vista Grande",
   },
 ];
 
