7c9f3e885ded6b11c870aab6827d2e68f3fac2cb
Quinn Peterson <qap2001@gmail.com>
2022-11-25 14:40:03 -0800

    home page restaurant search bar

--- frontend/src/RestaurantGrid.js
+++ frontend/src/RestaurantGrid.js
@@ -1,39 +1,54 @@
 //import {Container, Row, Col} from 'react-bootstrap';
-//import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import HeaderComp from "./header";
 //import Restaurant from "./Restaurant";
 //import { Routes, Route } from "react-router-dom";
 
-//===============================================================================
-
 function RestaurantGrid(props) {
   const restaurantData = props.restaurantData;
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
   function makeTableBody() {
-    const eles = restaurantData.map((restaurant) => {
-      const link = "http://localhost:3000/" + restaurant.name;
-      const linkStyle = { "text-decoration": "none" };
-      return (
-        <div key={restaurant._id} class="col-auto mb-3">
-          <a href={link} style={linkStyle}>
-            <div class="card" style={{ width: "18rem" }}>
-              <img src={restaurant.img} alt={restaurant.name} />
-              <div class="card-body">
-                <h5 class="card-title">{restaurant.name}</h5>
+    const reduced = restaurantData.reduce((accumulator, restaurant) => {
+      if (restaurant.name.toLowerCase().includes(searchInput.toLowerCase())) {
+        const link = "http://localhost:3000/" + restaurant.name;
+        const linkStyle = { "text-decoration": "none" };
+        accumulator.push(
+          <div key={restaurant._id} class="col-auto mb-3">
+            <a href={link} style={linkStyle}>
+              <div class="card" style={{ width: "18rem" }}>
+                <img src={restaurant.img} alt={restaurant.name} />
+                <div class="card-body">
+                  <h5 class="card-title">{restaurant.name}</h5>
+                </div>
               </div>
-            </div>
-          </a>
-        </div>
-      );
-    });
-    return eles;
+            </a>
+          </div>
+        );
+      }
+      return accumulator;
+    }, []);
+
+    return reduced;
   }
 
   return (
     <>
       <HeaderComp></HeaderComp>
-
       <div>
         <div class="container mt-4">
+          <input
+            type="text"
+            placeholder="Search here"
+            onChange={handleSearch}
+            value={searchInput}
+          />
+          <div class="row"></div>
           <div class="row">{makeTableBody()}</div>
         </div>
       </div>
