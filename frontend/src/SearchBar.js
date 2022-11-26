c082a2c755ff820027e0a66b7cbb1746b1c9970a
Aria-ista-0819 <aalva129@calpoly.edu>
2022-11-25 23:46:45 -0800

    added more functionality

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,36 +1,61 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
-import { BsSearch } from "react-icons/bs";
+//import { BsSearch } from "react-icons/bs";
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+//import { BsX } from "react-icons/bs";
+import Data from './data.json';
 
-function SearchBar({placeholder, data}) {
-
-  const {filteredData, setFilteredData} = useState([]);
+function SearchBar({placeholder,data}) {
+  //const restaurantData = props.restaurantData;
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchEntery, setSearchEntery] = useState("");
 
   const handleFilter = (event) => {
-    const searchWord = event.target.value
+    const searchWord = event.target.value;
+    setSearchEntery(searchWord)
     const newFilter = data.filter((value) => {
       return value.Restaurant.toLowerCase().includes(searchWord.toLowerCase);
     });
+
+    if(searchWord === ""){
+      setFilteredData([]);
+    }else{
     setFilteredData(newFilter);
+    }
   }
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setSearchEntery("");
+  }
+
   return (
     <div className='search'>
         <div className='searchInputs'>
             <input 
-              type="text" placeholder={placeholder} onChange={handleFilter}>
+              type="text"
+              placeholder={placeholder}
+              value={searchEntery}
+              onChange={handleFilter}
+              >
             </input>
-            <div className='BsSearch'><BsSearch/></div>
-        </div>
-        {/* {filteredData.length !== 0 && ( */}
-        <div className='dataResult'>
-          {data.map((value, key) => {
-            return <div className='dataItem'> 
-                <p>{value.Restaurant}</p> 
+            <div className='searchIcon'>
+              {filteredData.length === 0 ? 
+                (<SearchIcon/>) 
+                : 
+                (<CloseIcon id="clearBtn" onClick={clearInput} />)}
               </div>
+        </div>
+        {filteredData.length != 0 && (
+        <div className='dataResult'>
+          {filteredData.slice(0,3).map((value, key) => {
+            return <a className='dataItem'> 
+              <p>{value.Restaurant}</p> 
+              </a>
           })}
         </div>
-      {/* )} */}
+      )}
     </div>
   )
 }
--- frontend/src/header.js
+++ frontend/src/header.js
@@ -5,7 +5,8 @@ import { LinkContainer } from "react-router-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "./SearchBar";
 
-import Data from "./data.json";
+import Data from './data.json';
+
 
 const HeaderComp = () => {
   return (
@@ -13,23 +14,23 @@ const HeaderComp = () => {
       <script
         src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossOrigin="anonymous"
+        crossorigin="anonymous"
       ></script>
       <script
         src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossOrigin="anonymous"
+        crossorigin="anonymous"
       ></script>
       <script
         src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossOrigin="anonymous"
+        crossorigin="anonymous"
       ></script>
       <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossOrigin="anonymous"
+        crossorigin="anonymous"
       ></link>
 
       <div className="header">
@@ -46,9 +47,11 @@ const HeaderComp = () => {
               <Nav.Link>Submit Review</Nav.Link>
             </LinkContainer> */}
           </Nav>
+          <div class="ms-auto">
+            <SearchBar placeholder="Search..." data={Data}/>
+            </div>
         </Container>
       </Navbar>
-      {/* <SearchBar placeholder="Enter..." /> */}
     </div>
   );
 };
