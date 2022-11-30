2c9e3b335c38019b62fc0696ddca93474c997683
Quinn Peterson <qap2001@gmail.com>
2022-11-30 11:12:52 -0800

    temp comment out extra mukesh kumar review

--- frontend/src/RestaurantReviews.js
+++ frontend/src/RestaurantReviews.js
@@ -105,7 +105,8 @@ function RestaurantReviews(props) {
       </div>
 
       <ReviewBody />
-      <link
+
+      {/* <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
@@ -200,9 +201,9 @@ function RestaurantReviews(props) {
                 </div>
               </div>
             </div>
-          </div>
+          </div> 
         </div>
-      </div>
+      </div>*/}
     </div>
   );
 }
